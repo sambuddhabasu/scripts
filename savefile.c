2d7fa41af0f3105cfd279d835e26778868471bc4
Sambuddha Basu <sambuddhabasu1@gmail.com>
2014-12-08 15:50:38 +0400

    savefile.c works now

--- savefile.c
+++ savefile.c
@@ -3,7 +3,9 @@
    The script lets users save a file from the internet by specifying the URL
  */
 
+#include <arpa/inet.h>
 #include <errno.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,11 +14,14 @@
 #include <sys/types.h>
 
 #define MAXBUF		1024
+#define h_addr		h_addr_list[0]
 
 int main(int argc, char *argv[]) {
-	int sockfd, yes = 1, content_len, is_content, port;
-	char ip[MAXBUF], page[MAXBUF], buffer[MAXBUF], temp[MAXBUF], *token = NULL;
+	int sockfd, yes = 1, port, rc;
+	char ip[MAXBUF], ip_save[MAXBUF], page[MAXBUF], buffer[MAXBUF], temp[MAXBUF], request[MAXBUF], store_int[MAXBUF], *filename, *token = NULL;
 	struct sockaddr_in connect_addr;
+	struct hostent *he;
+	FILE *fp;
 	if(argc == 1) {
 		fprintf(stderr, "No URL provided\n");
 		exit(1);
@@ -27,7 +32,9 @@ int main(int argc, char *argv[]) {
 		sscanf(argv[1], "http://%99[^/]/%99[^\n]", ip, page);
 		port = 80;
 	}
-	printf("%s\n%d\n%s\n", ip, port, page);
+	strcpy(ip_save, ip);
+	filename = strrchr(page, '/');
+	filename++;
 	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
 		perror("socket");
 		exit(errno);
@@ -38,32 +45,39 @@ int main(int argc, char *argv[]) {
 	}
 	connect_addr.sin_family = AF_INET;
 	connect_addr.sin_port = htons(port);
-	if(inet_aton("192.168.0.102", &connect_addr.sin_addr.s_addr) == -1) {
-		perror("server");
-		exit(errno);
+	if(!inet_pton(AF_INET, ip, &(connect_addr.sin_addr.s_addr))) {
+		if((he = gethostbyname(ip)) == NULL) {
+			herror("gethostbyname");
+			exit(1);
+		}
+		strcpy(ip, inet_ntoa(*((struct in_addr *)he->h_addr)));
+		if(!inet_pton(AF_INET, ip, &(connect_addr.sin_addr.s_addr))) {
+			fprintf(stderr, "%s\n", ip);
+			exit(1);
+		}
 	}
 	memset(&(connect_addr.sin_zero), '\0', 8);
 	if(connect(sockfd, (struct sockaddr *)&connect_addr, sizeof(struct sockaddr)) == -1) {
 		perror("connect");
 		exit(errno);
 	}
-	bzero(buffer, MAXBUF);
-	send(sockfd, "GET / HTTP/1.1\r\nHost: 192.168.0.102\r\n\r\n", 39, 0);
-	recv(sockfd, buffer, sizeof(buffer), 0);
-	is_content = 0;
-	token = strtok(buffer, "\n");
-	while(token) {
-		if(sscanf(token, "Content-Length: %d", &content_len)) {
-			is_content = 1;
+	sprintf(request, "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", page, ip_save);
+	send(sockfd, request, strlen(request), 0);
+	recv(sockfd, buffer, MAXBUF, 0);
+	for(rc = 0; rc < MAXBUF - 4; rc++) {
+		if(buffer[rc] == '\r' && buffer[rc + 1] == '\n' && buffer[rc + 2] == '\r' && buffer[rc + 3] == '\n')
 			break;
-		}
-		token = strtok(NULL, "\n");
 	}
-	if(!is_content) {
-		fprintf(stderr, "Could not find Content-Length in the response headers\n");
-		exit(1);
+	rc += 4;
+	fp = fopen(filename, "wb");
+	while(rc < MAXBUF) {
+		fputc(buffer[rc], fp);
+		rc++;
 	}
-	printf("%d\n", content_len);
+	while((rc = recv(sockfd, buffer, MAXBUF, 0)) > 0) {
+		fwrite(buffer, rc, 1, fp);
+	}
+	fclose(fp);
 	close(sockfd);
 	return 0;
 }
